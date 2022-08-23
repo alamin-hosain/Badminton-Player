3e84886aa91a5b83ea827c3d182bd8c6905ec499
Hossain Alamin <hossainalamin@Hossains-MacBook-Pro.local>
2022-08-23 21:46:02 +0600

    finish checking error

--- js/app.js
+++ js/app.js
@@ -1,10 +1,18 @@
 
 
+// jokhhon disable hosche tokhon oie disable button a r notun kono event add hbena....
+
+
+
 
 document.getElementById('player-select').addEventListener('click', function (event) {
+    event.preventDefault();
+
     const selectPlayer = event.target.parentNode;
     const playerName = selectPlayer.querySelector('.card-title').innerText;
     const btnSelected = selectPlayer.querySelector('.btn');
+    btnSelected.disabled = 'disabled';
+
 
     const newPlayer = document.createElement('li');
     newPlayer.innerText = playerName;
@@ -15,7 +23,6 @@ document.getElementById('player-select').addEventListener('click', function (eve
 
     if (selecTedTotal.length < 5) {
         selectedPlayer.appendChild(newPlayer);
-        btnSelected.disabled = 'disabled';
 
     }
     else {
@@ -35,7 +42,20 @@ document.getElementById('calculate').addEventListener('click', function (event) 
     const budgetPerPlayer = getInputFieldValuById('budget-per-player');
     const playerExpense = budgetPerPlayer * selectedCurrentPlayer;
 
-    setElementValueByID('player-total-expense', playerExpense)
+
+    if (isNaN(budgetPerPlayer)) {
+
+        alert('You Forgot to Add a Valid Number');
+
+        const perPlayerInput = document.getElementById('budget-per-player');
+        perPlayerInput.value = '';
+
+    }
+    else {
+        setElementValueByID('player-total-expense', playerExpense)
+    }
+
+
 })
 
 
@@ -51,7 +71,19 @@ document.getElementById('calculate-total').addEventListener('click', function ()
     const managerAndCoachExpense = managerExpense + coachExpense;
     const totalExpense = playerTotalExpense + managerAndCoachExpense;
 
-    setElementValueByID('total-expense', totalExpense);
+    if (isNaN(managerExpense) && isNaN(coachExpense)) {
 
+        alert('You Forgot to Add a Valid Number');
+
+        const managerExpenseInput = document.getElementById('manager-expense');
+        managerExpenseInput.value = '';
+
+        const coachExpenseInput = document.getElementById('coach-expense');
+        coachExpenseInput.value = '';
+    }
+    else {
+
+        setElementValueByID('total-expense', totalExpense);
+    }
 
 })
